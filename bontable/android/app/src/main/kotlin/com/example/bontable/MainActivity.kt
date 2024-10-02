0e4510c1d444ff116e3952eac9b1f602f541d4c6
karayy <146695862+cKarayy@users.noreply.github.com>
2024-10-02 13:04:10 +0700

    project br

---
+++ bontable/android/app/src/main/kotlin/com/example/bontable/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bontable
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ bontable/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:bontable/screens/landing.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BonTable',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/landing': (context) => const LandingScreen(),
+      },
+    );
+  }
+}
